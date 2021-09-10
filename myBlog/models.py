61003c6b546c49420eb5e9fb882de265edc259bb
Haimei <1069022119@qq.com>
2021-09-10 10:54:50 +0800

    add a rich text editor to blog

--- myBlog/models.py
+++ myBlog/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.db.models.deletion import CASCADE
 from django.urls import reverse
+from ckeditor.fields import RichTextField
 
 
 class Category(models.Model):
@@ -19,7 +20,7 @@ class Post(models.Model):
     author = models.ForeignKey(User, on_delete=CASCADE, null=False)
     tag = models.CharField(max_length=255)
     category = models.CharField(max_length=50, default="coding")
-    body = models.TextField()
+    body = RichTextField(blank=True, null=True)
     post_time = models.DateTimeField(auto_now_add=True)
     likes = models.ManyToManyField(User, related_name="like_list")
 
